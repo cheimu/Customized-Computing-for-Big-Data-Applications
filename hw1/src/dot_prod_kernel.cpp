50ebd457ac978689872e9a13e697e77db11dd82f
Cloud User <centos@ip-172-31-24-36.us-west-1.compute.internal>
2019-10-08 23:45:14 +0000

    first commit

--- hw1/src/dot_prod_kernel.cpp
+++ hw1/src/dot_prod_kernel.cpp
@@ -11,10 +11,34 @@ void dot_prod_kernel(const float* a, const float* b, float* c, const int num_ele
 #pragma HLS interface s_axilite port = c bundle = control
 #pragma HLS interface s_axilite port = num_elems bundle = control
 #pragma HLS interface s_axilite port = return bundle = control
+#pragma HLS INLINE OFF  
   assert(num_elems <= 4096);  // this helps HLS estimate the loop trip count
   /***************************
    * your code goes here ... *
    ***************************/
+  float local_a[4096];
+  float local_b[4096];
+  for (int i = 0; i < num_elems; i++) {
+    local_a[i] = *a;
+    local_b[i] = *b;
+    a++;
+    b++; 
+  }  
+  float local_c[4096];
+#pragma HLS ARRAY_PARTITION variable=local_a block factor=4 dim=1
+#pragma HLS ARRAY_PARTITION variable=local_b block factor=4 dim=1
+#pragma HLS ARRAY_PARTITION variable=local_c block factor=4 dim=1
+  for (int i = 0; i < num_elems; i++) {
+#pragma HLS UNROLL
+    local_c[i] = local_a[i] * local_b[i];
+  }
+  
+  float res = 0;
+  for (int i = 0 ; i < num_elems; i++) {
+#pragma HLS PIPELINE
+    res = local_c[i] + res;
+  }
+  *c = res;
 }
 
 }  // extern "C"
